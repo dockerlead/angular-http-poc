2a7c76b6e73de48d7d007738b75171d08e4fde34
dockerlead <dockerlead@gmail.com>
2021-03-18 04:42:41 -0500

    Treat with response in interceptor

--- src/app/auth-interceptor.service.ts
+++ src/app/auth-interceptor.service.ts
@@ -1,14 +1,23 @@
 import {
   HttpInterceptor,
   HttpHandler,
-  HttpRequest
+  HttpRequest,
+  HttpEventType
 } from '@angular/common/http';
+import { tap } from 'rxjs/operators';
 
 export class AuthInterceptorService implements HttpInterceptor {
   intercept(req: HttpRequest<any>, next: HttpHandler) {
     console.log('Request is on its way...');
     console.log(req.url);
     const modifiedRequest = req.clone({headers: req.headers.append('Auth', 'xyz')});
-    return next.handle(modifiedRequest);
+    return next.handle(modifiedRequest).pipe(
+      tap(event => {
+        if (event.type === HttpEventType.Response) {
+          console.log('Response arrived, body data: ');
+          console.log(event.body);
+        }
+      })
+    );
   }
 }
