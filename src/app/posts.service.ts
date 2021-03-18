4ef3097f0055339f9a9563710068166149f4e02c
dockerlead <dockerlead@gmail.com>
2021-03-18 03:51:13 -0500

    Add query params, which is immutable object

--- src/app/posts.service.ts
+++ src/app/posts.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { map, catchError } from 'rxjs/operators';
 import { Observable, Subject, throwError } from 'rxjs';
 
@@ -27,12 +27,16 @@ export class PostsService {
   }
 
   fetchPosts(): Observable<Post[]> {
+    let searchParams = new HttpParams();
+    searchParams = searchParams.append('print', 'pretty');
+    searchParams = searchParams.append('custom', 'hello');
     return this.http.get<{ [key: string]: Post }>(
       'https://angular-udemy-course-recipe-default-rtdb.firebaseio.com/posts.json',
       {
         headers: new HttpHeaders({
           'Custom-Header': 'Hello'
-        })
+        }),
+        params: searchParams
       }
     )
     .pipe(
